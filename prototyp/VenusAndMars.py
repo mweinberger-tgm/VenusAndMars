e24bfddc2fdb5246c7b7195b5a6baf6c7180c487
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2015-12-08 19:00:30 +0100

    Unnütze Kommentare gelöscht

--- prototyp/VenusAndMars.py
+++ prototyp/VenusAndMars.py
@@ -1,12 +1,3 @@
-# Author: Shao Zhang and Phil Saltzman
-# Last Updated: 4/20/2005
-#
-# This tutorial will cover events and how they can be used in Panda
-# Specifically, this lesson will use events to capture keyboard presses and
-# mouse clicks to trigger actions in the world. It will also use events
-# to count the number of orbits the Earth makes around the sun. This
-# tutorial uses the same base code from the solar system tutorial.
-
 import direct.directbase.DirectStart
 from direct.showbase import DirectObject
 from panda3d.core import *
@@ -15,15 +6,7 @@ from direct.gui.DirectGui import *
 from direct.showbase.DirectObject import DirectObject
 import sys
 
-
-# We start this tutorial with the standard class. However, the class is a
-# subclass of an object called DirectObject. This gives the class the ability
-# to listen for and respond to events. From now on the main class in every
-# tutorial will be a subclass of DirectObject
-
 class World(DirectObject):
-    # Macro-like function used to reduce the amount to code needed to create the
-    # on screen instructions
     def genLabelText(self, text, i):
         return OnscreenText(text=text, pos=(-1.3, .95 - .05 * i), fg=(1, 1, 1, 1),
                             align=TextNode.ALeft, scale=.05, mayChange=1)
@@ -45,18 +28,6 @@ class World(DirectObject):
         self.loadPlanets()  # Load, texture, and position the planets
         self.rotatePlanets()  # Set up the motion to start them moving
 
-        # The standard title text that's in every tutorial
-        # Things to note:
-        # -fg represents the forground color of the text in (r,g,b,a) format
-        # -pos  represents the position of the text on the screen.
-        #      The coordinate system is a x-y based wih 0,0 as the center of the
-        #      screen
-        # -align sets the alingment of the text relative to the pos argument.
-        #      Default is center align.
-        # -scale set the scale of the text
-        # -mayChange argument lets us change the text later in the program.
-        #       By default mayChange is set to 0. Trying to change text when
-        #       mayChange is set to 0 will cause the program to crash.
         self.title = OnscreenText(text="Venus and Mars - Taschner | Weinberger 5BHIT",
                                   style=1, fg=(1, 1, 1, 1),
                                   pos=(0.9, -0.95), scale=.03)
@@ -65,22 +36,10 @@ class World(DirectObject):
         self.simRunning = True  # boolean to keep track of the
         # state of the global simulation
 
-        # Events
-        # Each self.accept statement creates an event handler object that will call
-        # the specified function when that event occurs.
-        # Certain events like "mouse1", "a", "b", "c" ... "z", "1", "2", "3"..."0"
-        # are references to keyboard keys and mouse buttons. You can also define
-        # your own events to be used within your program. In this tutorial, the
-        # event "newYear" is not tied to a physical input device, but rather
-        # is sent by the function that rotates the Earth whenever a revolution
-        # completes to tell the counter to update
         self.accept('escape', sys.exit)  # Exit the program when escape is pressed
 
     # end __init__
 
-    # toggleInterval does exactly as its name implies
-    # It takes an interval as an argument. Then it checks to see if it is playing.
-    # If it is, it pauses it, otherwise it resumes it.
     def toggleInterval(self, interval):
         if interval.isPlaying():
             interval.pause()
@@ -89,9 +48,6 @@ class World(DirectObject):
 
     # end toggleInterval
 
-    # Earth needs a special buffer function because the moon is tied to it
-    # When the "e" key is pressed, togglePlanet is called on both the earth and
-    # the moon.
     def handleEarth(self):
         self.togglePlanet("Earth", self.day_period_earth,
                           self.orbit_period_earth, self.ekeyEventText)
@@ -99,11 +55,6 @@ class World(DirectObject):
                           self.orbit_period_moon)
 
     # end handleEarth
-
-    #########################################################################
-    # Except for the one commented line below, this is all as it was before #
-    # Scroll down to the next comment to see an example of sending messages #
-    #########################################################################
 
     def loadPlanets(self):
         theme = loader.loadSfx("models/starwars.mp3")
