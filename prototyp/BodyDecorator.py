5148ce1f9e391a21ae2274c2b7e23849f0e6f92e
Thomas Taschner <ttaschner@student.tgm.ac.at>
2015-12-09 21:52:52 +0100

    Implemented decorator pattern

--- prototyp/BodyDecorator.py
+++ prototyp/BodyDecorator.py
@@ -1,8 +1,18 @@
+from src.Object import *
+from BodyFactory import *
 __author__ = 'Taschner | Weinberger'
 
 """
 Bestehende Objekte erweitern
 """
 
-class BodyDecorator(self):
-    def __init__(self):
+
+class BodyDecorator(Object):
+    def __init__(self, decoratee_name):
+        self.decoratee_name = decoratee_name
+
+        decorate_me_with = BodyFactory.create_object(decoratee_name)
+
+        self.orbit_root = (
+            self.decoratee_name.attachNewNode(decorate_me_with.orbit_root)
+        )
